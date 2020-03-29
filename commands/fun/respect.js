9206df8444f658f1cd3be4f7dc223f0cc7f655bb
maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2020-03-29 21:33:47 +0800

    Changed Embed from Author -> Desc

--- commands/fun/respect.js
+++ commands/fun/respect.js
@@ -7,7 +7,7 @@ const args2 = message.cleanContent.split(/ +/).slice(1)
 
 const desc = (!args.length) ? `${message.author.username} has paid their respects.` : `${message.author.username} has paid their respects to ${args2.join(' ')}`;
 const embed = new Discord.RichEmbed()
-    .setAuthor(desc)
+    .setDescription(desc)
     .setColor(settings.colors.embedDefault)
     .setFooter(`Press F to pay your respects.`);
     message.delete();
