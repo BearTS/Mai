10404c2cec6a5ba68b42b92a42390f5a06c177f4
Mai Sakurajima <sakurajimai.san1202@gmail.com>
2020-09-25 17:41:45 +0800

    Added support for longer lyrics via pagination.

--- commands/utility/lyrics.js
+++ commands/utility/lyrics.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
-const { MessageEmbed } = require('discord.js')
-const { TextHelpers: { textTrunctuate }} = require('../../helper')
+const { MessageEmbed, GuildEmoji } = require('discord.js')
+const { TextHelpers: { textTrunctuate }, Classes: { Paginate: page }} = require('../../helper')
 
 module.exports = {
   name: 'lyrics',
@@ -19,11 +19,73 @@ module.exports = {
 
     const { title, author, lyrics, thumbnail, links } = data
 
-    message.channel.send( new MessageEmbed()
+    if (lyrics.length < 2000)
+    return message.channel.send( new MessageEmbed()
       .setAuthor(`${title}\n${author}`, null, links.genius)
       .setThumbnail(thumbnail.genius)
-      .setDescription(lyrics.length < 2000 ? lyrics : `The lyrics exceeded the max character of 2000. **[Click here](${links.genius})** to view the lyrics in your browser instead.`)
+      .setDescription(lyrics)
       .setColor('GREY')
     )
+
+
+    const lyrics_array = lyrics.split('\n')
+    let n = 0
+    const lyrics_subarray = ['']
+
+    for (const line of lyrics_array){
+      if (lyrics_subarray[n].length + line.length < 2000){
+        lyrics_subarray[n] = lyrics_subarray[n] + line + '\n'
+      } else {
+        n++
+        lyrics_subarray.push(line)
+      }
+    }
+
+    const pages = new page()
+
+    for (const element of lyrics_subarray){
+      pages.add( new MessageEmbed()
+        .setAuthor(`${title}\n${author}`, null, links.genius)
+        .setThumbnail(thumbnail.genius)
+        .setDescription(element)
+        .setColor('GREY')
+        .setFooter(`Page ${pages.size === null ? 1 : pages.size + 1} of ${lyrics_subarray.length}`)
+      )
+    }
+
+    const msg = await message.channel.send(pages.currentPage)
+
+    const prev = client.emojis.cache.get('712581829286166579') || '◀'
+    const next = client.emojis.cache.get('712581873628348476') || '▶'
+    const terminate = client.emojis.cache.get('712586986216489011') || '❌'
+
+    const collector = msg.createReactionCollector( (reaction, user) => user.id === message.author.id)
+    const navigators = [ prev, next, terminate ]
+
+    for (let i = 0; i < navigators.length; i++) await msg.react(navigators[i])
+
+    let timeout = setTimeout(()=> collector.stop(), 180000)
+
+    collector.on('collect', async ( {emoji: {name}, users }) => {
+
+    switch(name){
+      case prev instanceof GuildEmoji ? prev.name : prev:
+        msg.edit(pages.previous())
+        break
+      case next instanceof GuildEmoji ? next.name : next:
+        msg.edit(pages.next())
+        break
+      case terminate instanceof GuildEmoji ? terminate.name : terminate:
+        collector.stop()
+        break
+      }
+
+      await users.remove(message.author.id)
+      timeout.refresh()
+
+    })
+
+    collector.on('end', async () => await msg.reactions.removeAll().catch(()=>null) ? null : null)
+
   }
 }
