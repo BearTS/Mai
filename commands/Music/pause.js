96f75f153ed7ee47af3403ff5d3a0d83678b6e61
BearTS <65192718+BearTS@users.noreply.github.com>
2020-10-13 15:57:38 +0530

    Creation Of Pause Command
    Pauses the Currently Playing Music

---
+++ commands/Music/pause.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../../utilities/music.js");
+
+module.exports = {
+  name: 'pause',
+  aliases: [],
+  guildOnly: true,
+  permissions: [],
+  clientPermissions: [],
+  group: 'music',
+  description: 'Pause the currently Playing Music',
+  examples: ['pause'],
+  parameters: [],
+  run:  async function (client, message, args) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (serverQueue && serverQueue.playing) {
+      serverQueue.playing = false;
+      serverQueue.connection.dispatcher.pause();
+      let xd = new MessageEmbed()
+      .setDescription("⏸ Paused the music for you!")
+      .setColor("YELLOW")
+      .setAuthor("Music has been paused!", "https://i.imgur.com/tT8YBSA.gif")
+      return message.channel.send(xd);
+    }
+    return sendError("There is nothing playing in this server.", message.channel);
+  },
+}
