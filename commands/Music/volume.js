6d63e917792612bd2065116e344942a0be3e1e6a
BearTS <65192718+BearTS@users.noreply.github.com>
2020-10-13 16:08:04 +0530

    Creation of volume.js
    Change Volume of the Bot

---
+++ commands/Music/volume.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../../utilities/music.js");
+module.exports = {
+  name: 'volume',
+  aliases: [],
+  guildOnly: true,
+  permissions: [],
+  clientPermissions: [],
+  group: 'music',
+  description: 'Change the volume of the Playing Music',
+  examples: ['volume [Number]'],
+  parameters: ['Number'],
+  run: async function (client, message, args) {
+    const channel = message.member.voice.channel;
+    if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+    if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
+    serverQueue.volume = args[0]; 
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+    let volemb = new MessageEmbed()
+    .setDescription(`I set the volume to: **${args[0]/5}/5**(divied by 5)`)
+    .setAuthor("Music Volume", "https://i.imgur.com/tT8YBSA.gif")
+    .setColor("BLUE")
+    return message.channel.send(volemb);
+  },
+}
