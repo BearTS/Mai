58b71034ed6031fe9d5cfec1b846cf496cec13ea
BearTS <65192718+BearTS@users.noreply.github.com>
2020-10-13 16:05:47 +0530

    Creation of Skip.js
    Command to skip the current playing music

---
+++ commands/Music/skip.js
@@ -0,0 +1,22 @@
+const sendError = require("../../utilities/music.js");
+
+module.exports = {
+  name: 'skip',
+  aliases: [],
+  guildOnly: true,
+  permissions: [],
+  clientPermissions: [],
+  group: 'music',
+  description: 'Skip The Current Playing Song',
+  examples: ['skip'],
+  parameters: [],
+  run:  async function (client, message, args) {
+    const channel = message.member.voice.channel
+    if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue)return sendError("There is nothing playing that I could skip for you.", message.channel);
+    serverQueue.connection.dispatcher.end("Skiped the music");
+    message.react("✅")
+  },
+}
+
