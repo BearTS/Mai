06a6d75e6b01a4e0a20a671de37cddd75936b130
Mai Sakurajima <sakurajimai.san1202@gmail.com>
2020-12-23 14:19:21 +0800

    (feat): Add guild join/leave notification

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,19 +1,6 @@
 const { MessageEmbed } = require('discord.js');
-const guilds = require(`${process.cwd()}/models/GuildProfile`);
-const consoleUtil = require(`${process.cwd()}/util/console`);
 
-module.exports = (client, guild) => guilds.findById(guild.id, async (err, doc) => {
-
-  const debug = client.channels.cache.get(client.config.channels.debug);
-
-  if (err && debug){
-    return debug.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: ${err.name}`);
-  } else if (!doc){
-    doc = await new guilds({ _id: guild.id }).save();
-  };
-
-  client.guildProfiles.set(guild.id, doc);
-
+module.exports = (client, guild) => {
   if (client.config.channels.logs){
     const channel = client.channels.cache.get(client.config.channels.logs);
     if (!channel){
@@ -24,7 +11,7 @@ module.exports = (client, guild) => guilds.findById(guild.id, async (err, doc) =
         .setTimestamp()
         .setColor('GREY')
         .setFooter(`ID: ${guild.id}`)
-        .setTitle(`Joined ${guild.name}!`)
+        .setTitle(`Left ${guild.name}!`)
         .setThumbnail(guild.iconURL({ format: 'png' }))
         .addFields([
           { name: '❯\u2000\u2000Members', value: guild.memberCount, inline: true },
@@ -33,4 +20,4 @@ module.exports = (client, guild) => guilds.findById(guild.id, async (err, doc) =
       );
     };
   };
-});
+}
