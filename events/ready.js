479795de79e9fa88e7767fc1c75a60a238049063
Mai Sakurajima <sakurajimai.san1202@gmail.com>
2020-12-23 14:20:26 +0800

    (chore): Remove unused var

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,7 @@
-const { performance } = require('perf_hooks');
 const consoleUtil = require(`${process.cwd()}/util/console`);
 
 module.exports = async client => {
-  
+
   consoleUtil.success(`${client.user.username} is now Online! (Loaded in ${client.bootTime} ms)\n\n`);
 
 };
