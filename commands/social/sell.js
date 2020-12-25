f01fc36cf68da6e31227a2c4d6bc33ddb572fc5c
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2020-12-25 17:14:48 +0800

    (feat): Add sell command

---
+++ commands/social/sell.js
@@ -0,0 +1,62 @@
+const profile = require(`${process.cwd()}/models/Profile`);
+const market = require(`${process.cwd()}/assets/json/market.json`);
+const text = require(`${process.cwd()}/util/string`);
+
+module.exports = {
+  name: 'sell',
+  aliases: [],
+  rankcommand: true,
+  clientPermissions: [ 'MANAGE_MESSAGES' ],
+  group: 'social',
+  description: 'Sell some of your items to the shop.',
+  run: (client, message, [id, amt] ) => profile.findById(message.author.id, async (err, doc) => {
+
+    if (err){
+      return message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: ${err.name}`);
+    } else if (!doc){
+      doc = new profile({ _id: message.author.id });
+    };
+
+    const item = market.find(x => x.id == id);
+
+    if (!item){
+      return message.channel.send(`\\❌ **${message.author.tag}**, Could not find the item with id ${id}!`);
+    };
+
+    amt = Math.floor(Math.abs(amt)) || 1;
+    const total = item.price * 0.7 * amt;
+    const itemcount = doc.data.profile.inventory.find(x => x.id === item.id)?.amount;
+
+    if (!itemcount || itemcount < amt){
+      return message.channel.send();
+    } else if (!item.price){
+      return message.channel.send(`\\❌ **${message.author.tag}**, Unable to sell ${item.name}.`);
+    } else {
+
+      const inv = doc.data.profile.inventory;
+      const old = inv.find(x => x.id === item.id);
+      let data = doc.data.profile.inventory.splice(inv.findIndex(x => x.id === old.id),1)[0];
+      data.amount = data.amount - amt;
+
+      if (data.amount > 0){
+        doc.data.profile.inventory.push(data);
+      } else {
+        // Do nothing
+      };
+
+      let overflow = false, excess = null;
+      if (doc.data.economy.wallet + total > 50000){
+        overflow = true;
+        excess = doc.data.economy.wallet + total - 50000
+      };
+
+      doc.data.economy.wallet = overflow ? 50000 : doc.data.economy.wallet + total;
+      return doc.save()
+      .then(() => message.channel.send([
+        `\\✔️ **${message.author.tag}**, successfully sold **${amt}x ${item.name}** for **${text.commatize(total)}**`,
+        overflow ? `\n⚠️Overflow warning! Please deposit some of your account to your **bank**. You only received ${text.commatize(total - excess)} for this one!` :'',
+      ].join('\n')))
+      .catch(() => message.channel.send(`\`❌ [DATABASE_ERR]:\` Unable to save the document to the database, please try again later!`));
+    };
+  })
+};
