9f5449f96906a4621e7b7510d76560172c80fa2f
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2020-12-28 22:19:55 +0800

    (fix): Item still used after being sold (#44)

--- commands/social/sell.js
+++ commands/social/sell.js
@@ -42,8 +42,8 @@ module.exports = {
 
       if (data.amount > 0){
         doc.data.profile.inventory.push(data);
-      } else if (item.assets.link === doc.data.profile[item.type]?.link) {
-        doc.data.profile[item.type] === null;
+      } else if (item.assets.link === doc.data.profile[item.type]) {
+        doc.data.profile[item.type] = null;
       } else {
         // Do nothing...
       };
