fcacc13730b4acf5f83d0ce27e41c49825d9d9ed
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2021-01-01 04:27:51 +0800

    (update): anime command UI

--- commands/anime/anime.js
+++ commands/anime/anime.js
@@ -1,5 +1,7 @@
+const moment = require('moment');
 const { getInfoFromName } = require('mal-scraper');
 const { MessageEmbed } = require('discord.js');
+const { malGenres } = require(`${process.cwd()}/util/constants`);
 const text = require(`${process.cwd()}/util/string`);
 
 module.exports = {
@@ -22,80 +24,90 @@ module.exports = {
 
     const query = args.join(' ') || 'Seishun Buta Yarou';
 
-    const embed = new MessageEmbed()
-    .setColor('YELLOW')
-    .setThumbnail('https://i.imgur.com/u6ROwvK.gif')
-    .setDescription(`Searching for Anime titled **${query}** on <:mal:767062339177676800> [MyAnimeList](https://myanimelist.net 'Homepage').`)
-    .setFooter(`Anime Query with MAL | \©️${new Date().getFullYear()} Mai`);
-
-    const msg = await message.channel.send(embed);
+    // Indicate that the bot is doing something in the background
+    message.channel.startTyping();
 
     const data = await new Promise((resolve,reject) => {
-      const timer = setTimeout(() => reject('TIMEOUT'), 10000)
+      setTimeout(() => reject('TIMEOUT'), 10000);
 
       return getInfoFromName(query)
       .then(res => resolve(res))
       .catch(err => reject(err));
     }).catch((err)=> err !== 'TIMEOUT' ? null : err)
 
-    embed.setColor('RED')
-    .setThumbnail('https://i.imgur.com/qkBQB8V.png')
-    .setDescription(
-      !data
-      ? `**${message.member.displayName}**, No results were found for **${query}**!\n\n`
-      + `If you believe this anime exists, try the following methods:\n`
-      + `• Try the alternative names (e.g. English, Native, Romaji).\n`
-      + `• Include the season number (if it exists).\n`
-      + `• Include the term 'OVA' if it's an OVA.\n`
-      : 'MyAnimeList.net took long to respond (Timeout)\n\n'
-      + `Please try again in a few minutes. This is usually caused by a server downtime.`
-    ).setAuthor(!data ? 'None Found' : 'Response Error','https://cdn.discordapp.com/emojis/767062250279927818.png?v=1');
-
-    if (!data || data === 'TIMEOUT'){
-      return await msg.edit(embed).catch(() => null) || message.channel.send(embed);
+    if (!data){
+      return message.channel.send([
+        `\\❌ **${message.author.tag}**, No results were found for **${query}**`,
+        'If you believe this anime exists, try the following methods:',
+        '\u2000•\u2000Try the alternative names (e.g. English, Native, Romanized)',
+        '\u2000•\u2000Include the season number (if applicable)',
+        '\u2000•\u2000Include the type (e.g. OVA, ONA, TV Shorts).'
+      ].join('\n')).then(() => channel.stopTyping());
+    } else if (data === 'TIMEOUT'){
+      return message.channel.send([
+        `\\❌ **${message.author.tag}**, MyAnimeList took longer to respond.`,
+        'Please try again later, this may be caused by a server downtime.'
+      ].join('\n')).then(() => channel.stopTyping());
     };
 
-    embed.setColor('GREY')
-    .setFooter(`Anime Query with MAL | \©️${new Date().getFullYear()} Mai`)
-    .setThumbnail(data.picture || null)
-    .setAuthor([
-      text.truncate(data.englishTitle || data.title, 200),
-      text.truncate(data.type || 'showType Unavailable', 200)
-    ].join('\u2000|\u2000'), data.picture || 'https://myanimelist.net/images/icon.svg', data.url)
-    .setDescription([
-      `Score: ${data.score}\u2000•\u2000Ranked: ${text.ordinalize((data.ranked || 0).slice(1).replace('0th','~'))}\u2000•\u2000Popularity: ${data.popularity || '~'}`,
-      `${text.truncate(data.synopsis || 'No Synopsis', 1000, `... [Read More](${data.url})`)}`
-    ].join('\n\n'))
-    .addFields([
-      {
-        name: '<:info:767062326859268116>  Information',
-        value: [
-          `•\u2000\**Japanese Name:**\u2000[${data.title}](${data.url})`,
-          `•\u2000\**Age Rating:**\u2000${data.rating || 'Unrated'}`,
-          `•\u2000\**Source:**\u2000${data.source || 'N/A'}`
-        ].join('\n'),
-        inline: true
-      },
-      {
-        name: '\u200b',
-        value: [
-          `•\u2000\**Genres:**\u2000${text.joinArray(data.genres) || ''}`,
-          `•\u2000\**Producers:**\u2000${text.joinArray(data.producers) || ''}`,
-          `•\u2000\**Studios:**\u2000${text.joinArray(data.studios) || ''}`
-        ].join('\n'),
-        inline: true
-      },
-      {
-        name: '<:stats:767062320425730059>  Status',
-        value: [
-          `•\u2000\**Episodes:**\u2000${data.episodes || 'Unknown'}`,
-          `•\u2000\**Start Date:**\u2000${data.aired || 'Unknown'}`,
-          `•\u2000\**Status:**\u2000${data.status || 'Unknown'}`
-        ].join('\n')
-      }
-    ]);
-
-
-    return await msg.edit(embed).catch(()=>{}) || message.channel.send(embed);
+    message.channel.stopTyping();
+    
+    return message.channel.send(
+      new MessageEmbed()
+      .setColor('GREY')
+      .setThumbnail(data.picture || null)
+      .setFooter(`Anime Query with MAL | \©️${new Date().getFullYear()} Mai`)
+      .setAuthor([
+        text.truncate(data.englishTitle || data.title, 200),
+        text.truncate(data.type || 'showType Unavailable', 200)
+      ].join('\u2000|\u2000'), null, data.url)
+      .setDescription([
+        data.japaneseTitle,
+        [
+          `[\\⭐](https://myanimelist.net/info.php?go=topanime 'Score'): **${data.score}**`,
+          `[\\🏅](https://myanimelist.net/info.php?go=topanime 'Rank'): **${text.ordinalize((data.ranked.replace('N/A','0')).slice(1)).replace(/0th/,'N/A')}**`,
+          `[\\✨](https://myanimelist.net/info.php?go=topanime 'Popularity'): **${data.popularity || '~'}**`,
+          `[\` ▶ \`](${data.trailer} 'Watch Trailer')`
+        ].join('\u2000\u2000•\u2000\u2000'),
+        `\n${text.joinArray(data.genres.map(g =>
+          `[**${g}**](https://myanimelist.net/anime/genre/${malGenres[g.toLowerCase()]})`
+        )||[])}`,
+        '━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+      ].filter(Boolean).join('\n'))
+      .addFields([
+        {
+          name: '\\🔸 Source', inline: true,
+          value: '\u2000' + data.source || 'Unknown'
+        },{
+          name: '\\🔸 Episodes', inline: true,
+          value: data.episodes || 'Unknown'
+        },{
+          name: '\\🔸 Duration', inline: true,
+          value: data.duration || 'Unknown'
+        },{
+          name: '\\🔸 Members', inline: true,
+          value: data.members || 'Unknown'
+        },{
+          name: '\\🔸 Favorites', inline: true,
+          value: data.favorites || 'Unknown'
+        },{
+          name: '\\🔸 Studio', inline: true,
+          value: data.studios?.[0] || 'Unknown'
+        },{
+          name: `\\🕐 ${data.status === 'Finished Airing' ? 'Aired' : 'Airs'} (*${moment(data.aired.split('to')[0], 'll').fromNow()}*)`,
+          value: data.aired || 'Unknown'
+        },{
+          name: '\\🎬 Producers',
+          value: text.joinArray(data.producers||[]) || 'Unknown'
+        },{
+          name: '━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+          value: [
+            text.truncate(data.synopsis||'No Synopsis', 500, `...\n\n[\`📖\`](${data.url} 'Read More on MyAnimeList')`) || 'No Synopsis Available.',
+            '━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+            `**${data.rating.replace('None', '') || 'Unrated'}**`
+          ].join('\n')
+        }
+      ])
+    );
   }
 };
