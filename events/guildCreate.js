8c6f0c473b317da67788453872f5abab8210d033
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2021-01-02 23:21:52 +0800

    (chore): Change guild join notification UI

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,24 +4,53 @@ const text = require(`${process.cwd()}/util/string`);
 
 module.exports = (client, guild) => guilds.findById(guild.id, async (err, doc) => {
 
-  const debug = client.channels.cache.get(client.config.channels.debug);
-  const owner = await client.users.fetch(guild.ownerID);
+  /*===============WELCOME TO THE GUILD_CREATE EVENT=============
+    This function runs everytime the bot receives any guild payload
+    from discord after the ready event is fired.
+  =============================================================*/
 
-  if (err && debug){
-    return debug.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: ${err.name}`);
-  } else if (!doc){
-    doc = await new guilds({ _id: guild.id }).save();
-  };
+  /*=====================================================
+     Declare variables
+  =====================================================*/
+  const owner = await client.users.fetch(guild.ownerID)
+  .then(owner => owner.tag)
+  .catch(() => '<Unfetched Data>');
 
-  client.guildProfiles.set(guild.id, doc);
+  const logo = '<:Enter:794918219835637760>';
+  const members = text.commatize(guild.memberCount);
+  const message = `${logo} : **${members}** members, owned by **${owner}**`;
+  //====================================================//
 
-  if (client.config.channels.logs){
-    const channel = client.channels.cache.get(client.config.channels.logs);
-    if (!channel){
-      return;
-    } else {
-      channel.send(`**JOIN** \`[ ${guild.id} ]\` **${guild.name}** (Owned by **${owner.tag}**): ${text.commatize(guild.memberCount)} members.`)
-      .catch(() => {});
+
+  /*======================================================
+     Check the validity of database connection and save
+     new guild profile.
+  ======================================================*/
+  if (err){
+    client.channels.cache.get(client.config.channels.debug)?.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: ${err.name}`);
+  } else {
+    if (!doc){
+      doc = await new guilds({ _id: guild.id }).save();
     };
+    client.guildProfiles.set(guild.id, doc);
   };
+  //====================================================//
+
+
+
+  /*======================================================
+     Sends a notification to a log channel (if available)
+     that the bot has joined a server
+  ======================================================*/
+  await client.channels.cache.get(client.config.channels?.logs)?.createWebhook(guild.name, {
+    avatar: guild.iconURL({ format: 'png', dynamic: true, size: 128 })
+  })
+  .then(webhook => Promise.all([webhook.send(message), webhook]))
+  .then(([_, webhook]) => webhook.delete())
+  .catch(() => {});
+  //=====================================================//
+
+  // add more functions on message guildCreate callback function...
+
+  return;
 });
