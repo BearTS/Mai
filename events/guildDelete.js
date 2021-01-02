3bb44a02fcf71d6d9dbd8a3e665d50720e71bec6
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2021-01-02 23:26:55 +0800

    (chore): Log messages UI changes (#58)
    * (chore): Change reboot UI
    
    * (chore): Change guild join notification UI
    
    * (chore): Change guild leave notification UI
    
    * (chore): remove debug traces

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,14 +1,37 @@
 const text = require(`${process.cwd()}/util/string`);
 
 module.exports = async (client, guild) => {
-  if (client.config.channels.logs){
-    const owner = await client.users.fetch(guild.ownerID);
-    const channel = client.channels.cache.get(client.config.channels.logs);
-    if (!channel){
-      return;
-    } else {
-      channel.send(`**LEAVE** \`[ ${guild.id} ]\` **${guild.name}** (Owned by **${owner.tag}**): ${text.commatize(guild.memberCount)} members.`)
-      .catch(() => {});
-    };
-  };
-}
+
+  /*===============WELCOME TO THE GUILD_CREATE EVENT=============
+    This function runs everytime the bot leaves a guild
+    from discord after the ready event is fired.
+  =============================================================*/
+
+  /*=====================================================
+     Declare variables
+  =====================================================*/
+  const owner = await client.users.fetch(guild.ownerID)
+  .then(owner => owner.tag)
+  .catch(() => '<Unfetched Data>');
+
+  const logo = '<:leave:794918240651706389>';
+  const members = text.commatize(guild.memberCount);
+  const message = `${logo} : **${members}** members, owned by **${owner}**`;
+  //===================================================//
+
+  /*======================================================
+     Sends a notification to a log channel (if available)
+     that the bot has left a server
+  ======================================================*/
+  await client.channels.cache.get(client.config.channels?.logs).createWebhook(guild.name, {
+    avatar: guild.iconURL({ format: 'png', dynamic: true, size: 128 })
+  })
+  .then(webhook => Promise.all([webhook.send(message), webhook]))
+  .then(([_, webhook]) => webhook.delete())
+  .catch(() => {});
+  //=====================================================//
+
+  // add more functions on guildDelete event callback function...
+
+  return;
+};
