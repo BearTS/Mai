d9713bb5b4ef1c019836f20a160fea7396e30c1d
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2021-01-10 04:21:09 +0800

    (refactor): add streak on bal command

--- commands/social/bal.js
+++ commands/social/bal.js
@@ -31,7 +31,7 @@ module.exports = {
           : `Seems like you don't have a bank yet. Create one now by typing \`${
             client.config.prefix
           }bank\``
-        }`
+        }\n\nDaily Streak: **${doc.data.economy.streak.current}** (All time best: **${doc.data.economy.streak.alltime}**)`
       ).setAuthor(`${message.member.displayName}'s wallet`)
       .setColor('GREY')
       .setThumbnail(message.author.displayAvatarURL({dynamic: 'true'}))
