eecd69cdb1deea864bf029aa8cc39a3ef3880a9f
Sakurajimai#6742 <56829176+maisans-maid@users.noreply.github.com>
2021-01-10 04:20:52 +0800

    (update): rename coins to credits on bal command.

--- commands/social/bal.js
+++ commands/social/bal.js
@@ -25,9 +25,9 @@ module.exports = {
       new MessageEmbed().setDescription(
         `\u200B\n💰 **${
           text.commatize(doc.data.economy.wallet)
-        }** coins in posession.\n\n${
+        }** credits in posession.\n\n${
           doc.data.economy.bank !== null
-          ? `💰 **${text.commatize(doc.data.economy.bank)}** coins in bank!`
+          ? `💰 **${text.commatize(doc.data.economy.bank)}** credits in bank!`
           : `Seems like you don't have a bank yet. Create one now by typing \`${
             client.config.prefix
           }bank\``
