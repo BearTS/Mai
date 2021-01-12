ff448ac856aa2d734c12aa45ada1c7bf82c37523
DJbeast2 <71985983+DJbeast2@users.noreply.github.com>
2021-01-12 04:02:45 -0500

    actions

---
+++ commands/action/wave.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js')
+const { wave } = require('../../assets/json/images.json')
+
+module.exports = {
+    name: 'wave'
+  , aliases: []
+  , guildOnly: true
+  , clientPermissions: [
+    'EMBED_LINKS',
+    'ADD_REACTIONS'
+  ]
+  , group: 'action'
+  , description: 'UWAA~!'
+  , examples: [
+      'wave'
+  ]
+  , parameters: []
+  , run: async ( client, message, args ) => {
+
+    return message.channel.send( new MessageEmbed()
+    .setColor('GREY')
+    .setDescription(`wave.`)
+    .setImage(`https://i.imgur.com/${wave[Math.ceil(Math.random() * wave.length)]}.gif`)
+    .setFooter(`Action Commands | \©️${new Date().getFullYear()} Mai`)
+    )
+  }
+}
