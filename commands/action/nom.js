818b1f3182c719799d873b6a41504a7f5b9b4a5d
Syvel Henderson <syveljr0924@yahoo.com>
2021-01-12 03:55:45 -0500

    More actions

---
+++ commands/action/nom.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js')
+const { nom } = require('../../assets/json/images.json')
+
+module.exports = {
+    name: 'nom'
+  , aliases: []
+  , guildOnly: true
+  , clientPermissions: [
+    'EMBED_LINKS',
+    'ADD_REACTIONS'
+  ]
+  , group: 'action'
+  , description: 'UWAA~!'
+  , examples: [
+      'nom'
+  ]
+  , parameters: []
+  , run: async ( client, message, args ) => {
+
+    return message.channel.send( new MessageEmbed()
+    .setColor('GREY')
+    .setDescription(`nom.`)
+    .setImage(`https://i.imgur.com/${nom[Math.ceil(Math.random() * nom.length)]}.gif`)
+    .setFooter(`Action Commands | \©️${new Date().getFullYear()} Mai`)
+    )
+  }
+}
---
+++ commands/action/punch.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const { punch } = require("../../assets/json/images.json");
+
+module.exports = {
+  name: "punch",
+  aliases: [],
+  guildOnly: true,
+  clientPermissions: ["EMBED_LINKS", "ADD_REACTIONS"],
+  group: "action",
+  description: "UWAA~!",
+  examples: ["punch"],
+  parameters: [],
+  run: async (client, message, args) => {
+    return message.channel.send(
+      new MessageEmbed()
+        .setColor("GREY")
+        .setDescription(`${message.mentions.members.first()} got punched by ${message.member}! rip!`)
+        .setImage(
+          `https://i.imgur.com/${
+            punch[Math.ceil(Math.random() * punch.length)]
+          }.gif`
+        )
+        .setFooter(`Action Commands | \©️${new Date().getFullYear()} Mai`)
+        )
+    );
+  }
+};
