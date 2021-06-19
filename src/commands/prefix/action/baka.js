1d67fd4bfbbe1e57d44bc94fae9f587147d256d6
BearTS <bear@tamako.tech>
2021-06-19 16:52:25 -0700

    Update baka.js

--- src/commands/prefix/action/baka.js
+++ src/commands/prefix/action/baka.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const { MessageEmbed, Permissions: { FLAGS }} = require('discord.js');
 
 module.exports = {
@@ -19,32 +20,39 @@ module.exports = {
   run              : async (message, language, args) => {
 
     const parameters = new language.Parameter({ '%AUTHOR%': message.author.tag });
+    const res = await fetch(`https://api.tamako.tech/api/roleplay/baka`)
+    .then(res => res.json())
+    .catch(() => {});
+    const embed = new MessageEmbed()
+              .setColor('GREY')
+              .setImage(res.url)
+              .setFooter(`Action Commands | \©️${new Date().getFullYear()} Mai`);
     let   member;
     if (!args){
-
     };
     if (!args.join(' ').match(/\d{17,19}/)){
       const members = await message.guild.members.fetch({ query: args.join(' '), limit: 5 }).catch(err => {});
       if (!members.size){
-
+         return message.channel.send(embed);
       };
       if (members.size > 1){
-
+         return message.channel.send(embed);
       };
       member = members.first();
     };
     if (args.join(' ').match(/\d{17,19}/)){
       member = await message.guild.members.fetch(args.join(' ').match(/\d{17,19}/)[0]).catch(err => err);
       if (member instanceof Error){
-
+        return message.channel.send("error")
       };
     };
     if (member.id === message.client.user.id){
-
+      return message.react('💢');
     };
     if (member.id === message.author.id){
-
+      return message.channel.send(`\\❌ No **${message.author.tag}**, you're not Baka!`);
     };
-    return;
+    return message.channel.send(
+    embed.setDescription(`${args[0]} B~baka!`));
   }
 };
